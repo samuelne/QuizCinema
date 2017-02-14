d70c0c3de873b73e49f1037c5b15e3e2af9827d9
Samuel Nunes <samuelprok@gmail.com>
2017-02-14 12:54:04 -0200

    Criação da conexão do banco de dados, e algumas correções foram feitas no protótipo

--- QuizCinema/QuizCinema/MenuInicial.cs
+++ QuizCinema/QuizCinema/MenuInicial.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuizCinema
 {
@@ -19,7 +20,16 @@ namespace QuizCinema
 
         private void btnComecar_Click(object sender, EventArgs e)
         {
-            
+            using (SqlConnection conexao = new SqlConnection("Server = AME0556341W10-1\\SQLEXPRESS;Database=db_QuizCinema;Trusted_Connection=Yes"))
+            {
+                using (SqlCommand cmd = new SqlCommand("insert into tb_pergunta(id_jogador) OUTPUT INSERTED.ID values(@ID_JOGADOR)", conexao))
+                {
+
+
+                }
+
+            }
+
             TelaBoasVindas boasVindas = new TelaBoasVindas();
             boasVindas.Show();
             this.Hide();
--- QuizCinema/QuizCinema/Pergunta1.cs
+++ QuizCinema/QuizCinema/Pergunta1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuizCinema
 {
@@ -19,6 +20,16 @@ namespace QuizCinema
 
         private void btnPerg1_Click(object sender, EventArgs e)
         {
+            using (SqlConnection conexao = new SqlConnection("Server = AME0556341W10-1\\SQLEXPRESS;Database=db_QuizCinema;Trusted_Connection=Yes"))
+            {
+                using (SqlCommand cmd = new SqlCommand("insert into tb_pergunta(id_jogador) OUTPUT INSERTED.ID values(@ID_JOGADOR)", conexao))
+                {
+                    
+
+                }
+
+            }
+                        
             Pergunta2 perg2 = new Pergunta2();
             perg2.Show();
             this.Close();
